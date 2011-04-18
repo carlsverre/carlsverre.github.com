3c1d34c8bcf152b280d7dc01524516555d894f0f
Carl Sverre <mail@carlsverre.com>
2011-04-17 18:00:36 -0700

    Cleaned up app.js a bit

--- app.js
+++ app.js
@@ -2,34 +2,7 @@
 
 var debugMode = false;
 
-function init() {
-	$('.boxgrid.caption').hover(function() {
-		$('.cover', this).stop().animate({top:'181px'},{queue:false,duration:160});
-	}, function() {
-		$('.cover', this).stop().animate({top:'200px'},{queue:false,duration:160});
-	});
-	
-	$('.mixpanel-track-click').each(function(i, link) {
-		link.setAttribute("onClick", "javascript: trackAndGo('"+link.href+"', '"+link.title+"')");
-		link.href="javascript:void(0);";
-	});
-	
-	var referrer = parseUri(document.referrer);
-	debugMode = (referrer && referrer.host == 'localhost');
-
-	// track the visiting event
-	if(referrer) {
-		mpmetrics.register({
-			'referrer': referrer.host,
-			'mp_source': referrer.host
-		});
-		
-		if(!debugMode) mpmetrics.track("visit");
-	}
-}
-
 function trackAndGo(link, title) {
-	if(debugMode) return;
 	mpmetrics.track("clicked_skill", {
 		'skill': title
 	}, function() {
@@ -47,15 +20,15 @@ function parseUri (str) {
 		uri = {},
 		i   = 14;
 
-	while (i--) uri[o.key[i]] = m[i] || "";
+	while (i--) { uri[o.key[i]] = m[i] || ""; }
 
 	uri[o.q.name] = {};
 	uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
-		if ($1) uri[o.q.name][$1] = $2;
+		if ($1) { uri[o.q.name][$1] = $2; }
 	});
 
 	return uri;
-};
+}
 
 parseUri.options = {
 	strictMode: false,
@@ -69,3 +42,34 @@ parseUri.options = {
 		loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
 	}
 };
+
+// end parseUri
+
+function init() {
+	$('.boxgrid.caption').hover(function() {
+		$('.cover', this).stop().animate({top:'181px'},{queue:false,duration:160});
+	}, function() {
+		$('.cover', this).stop().animate({top:'200px'},{queue:false,duration:160});
+	});
+	
+	var loc = parseUri(location.host);
+	debugMode = (loc && loc.host === 'localhost');
+
+	// track the visiting event
+	if(!debugMode) {
+		$('.mixpanel-track-click').each(function(i, link) {
+			$(link).click(function(e) {
+				trackAndGo(link.href,link.title);
+				e.preventDefault();
+			});
+			link.href = "#";
+		});
+		
+		var referrer = parseUri(document.referrer);
+		mpmetrics.register({
+			'referrer': referrer.host,
+			'mp_source': referrer.host
+		});
+		mpmetrics.track("visit");
+	}
+}
