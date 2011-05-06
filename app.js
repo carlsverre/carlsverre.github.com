9a0f279e5dfbad89a31d5b93d3684eb49ffd8a80
Carl Sverre <csverre@mixpanel.com>
2011-05-06 15:42:21 -0700

    Modified tracking code a bit

--- app.js
+++ app.js
@@ -3,47 +3,47 @@
 var debugMode = false;
 
 function trackAndGo(link, title) {
-	mpmetrics.track("clicked_skill", {
+	mpmetrics.track(debugify("clicked_skill"), {
 		'skill': title
 	}, function() {
 		window.location = link;
 	});
 }
 
-// parseUri 1.2.2
-// (c) Steven Levithan <stevenlevithan.com>
-// MIT License
-
-function parseUri (str) {
-	var	o   = parseUri.options,
-		m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
-		uri = {},
-		i   = 14;
-
-	while (i--) { uri[o.key[i]] = m[i] || ""; }
-
-	uri[o.q.name] = {};
-	uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
-		if ($1) { uri[o.q.name][$1] = $2; }
-	});
-
-	return uri;
+function browser() {
+    var a = navigator.userAgent;
+    if (window.opera) {
+        return "Opera";
+    } else if (/chrom/i.test(a)) {
+        return "Chrome"; 
+    } else if (/msie/i.test(a)) {
+        return "Internet Explorer";
+    } else if (/AppleWebKit/.test(navigator.appVersion)) {
+        return "Safari";
+    } else if (/mozilla/i.test(a) && !/compatible|webkit/i.test(a)) {
+        return "Firefox";
+    } else {
+        return "";
+    }
 }
 
-parseUri.options = {
-	strictMode: false,
-	key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
-	q:   {
-		name:   "queryKey",
-		parser: /(?:^|&)([^&=]*)=?([^&]*)/g
-	},
-	parser: {
-		strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
-		loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
-	}
-};
+function os() {
+    var a = navigator.userAgent;
+    if (/Windows/i.test(a)) {
+        return 'Windows';
+    } else if (/iPhone/.test(a)) {
+        return 'iPhone';
+    } else if (/Android/.test(a)) {
+        return 'Android';
+    } else if (/Mac/i.test(a)) {
+        return 'Mac OS X';
+    } else if (/X11/.test(a) || /Linux/.test(a)) {
+        return 'Linux';
+    } else {
+        return '';   
+    }
+}
 
-// end parseUri
 function get_pageview_info() {
     var mp_referrer = document.referrer,
         mp_browser = browser(),
@@ -51,12 +51,17 @@ function get_pageview_info() {
         mp_page = document.location.href,
         props = {};
 
-    if (mp_referrer.length) { props.mp_referrer = mp_referrer } 
+    if (mp_referrer.length) { props.mp_referrer = props.referrer = mp_referrer }
     if (mp_browser.length) { props.mp_browser = mp_browser } 
     if (mp_platform.length) { props.mp_platform = mp_platform } 
     if (mp_page.length) { props.mp_page = mp_page } 
 
     return props;
+}
+
+function debugify(evt) {
+    if(debugMode) return evt + "_debug";
+    else return evt;
 }
 
 function init() {
@@ -66,24 +71,16 @@ function init() {
 		$('.cover', this).stop().animate({top:'200px'},{queue:false,duration:160});
 	});
 	
-	var loc = parseUri(location.host);
-	debugMode = (loc && loc.host === 'localhost');
+	debugMode = location.host.search('localhost') != -1;
 
 	// track the visiting event
-	if(!debugMode) {
-		$('.mixpanel-track-click').each(function(i, link) {
-			$(link).click(function(event) {
-				event.preventDefault();
-				trackAndGo(link.href,link.title);
-			});
-		});
-		
-//		var referrer = parseUri(document.referrer);
-//		mpmetrics.register({
-//			'referrer': referrer.host,
-//			'mp_source': referrer.host
-//		});
-//		mpmetrics.track("visit");
-        mpmetrics.track("mp_page_view", get_pageview_info());
-	}
+    $('.mixpanel-track-click').each(function(i, link) {
+        $(link).click(function(event) {
+            event.preventDefault();
+            trackAndGo(link.href,link.title);
+        });
+    });
+    
+    mpmetrics.register(get_pageview_info());
+    mpmetrics.track(debugify("visit"));
 }
